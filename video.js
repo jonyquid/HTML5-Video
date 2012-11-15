f6bacfcb2e87d30ddfce0bba464553d880da41ae
Samet Alparslan <cellf1sh@Samet-Alparslans-MacBook-Pro.local>
2012-11-15 21:42:34 +0000

    video.js
    + /* Generate preview using JavaScript variables for video and canvas
    elements
    +
    + By Samet Kohen
    + */
    + 	function generatePreview() {
    + 		var video = document.getElementById('originalVideo');
    + 		var canvas1 = document.getElementById('previewMedium');
    + 		var context1 = canvas1.getContext('2d');
    + 		var canvas2 = document.getElementById('previewsSmall');
    + 		var context2 = canvas2.getContext('2d');
    + 	}

---
+++ video.js
@@ -0,0 +1,11 @@
+/* Generate preview using JavaScript variables for video and canvas elements
+
+By Samet Kohen
+*/
+	function generatePreview() {
+		var video = document.getElementById('originalVideo');
+		var canvas1 = document.getElementById('previewMedium');
+		var context1 = canvas1.getContext('2d');
+		var canvas2 = document.getElementById('previewsSmall');
+		var context2 = canvas2.getContext('2d');
+	}
