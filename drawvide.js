2ba5e4a66cc33ee520dfd399d2e854fe9b3c9546
Samet Alparslan <cellf1sh@Samet-Alparslans-MacBook-Pro.local>
2012-11-15 21:51:04 +0000

    drawvideo.js
    /*
    Draw image with scaling to draw a canvas image that is half the size of
    the original video.
    Function to check if video is playing with scaling parameters.
    
    By Samet Kohen
    
    */
    
    	function drawVideo(video,canvas1,canvas2) {
    		if(video.paused || video.ended) return false;
    		canvas1.drawImage(video,0,0,320,180);
    		canvas2.drawImage(video,0,0,160,90);
    		timeoutSet(drawVideos,25,video,canvas1,canvas2);
    	}

--- drawvide.js
+++ drawvide.js
@@ -1,3 +1,11 @@
+/*
+Draw image with scaling to draw a canvas image that is half the size of the original video.
+Function to check if video is playing with scaling parameters.
+
+By Samet Kohen
+
+*/
+	
 	function drawVideo(video,canvas1,canvas2) {
 		if(video.paused || video.ended) return false;
 		canvas1.drawImage(video,0,0,320,180);
